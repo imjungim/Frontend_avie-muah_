a7b2a56b5f8e7d48d0ebe569a3a8a974b5c7ae59
imjungim <jungim4201@gmail.com>
2023-02-23 22:51:54 +0900

    ♻️ header,main background, footer markup and style edit

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,22 @@
-const menuBtn = document.querySelector('.menu-button');
-const sidebar = document.querySelector('.sidebar');
-const overlay = document.querySelector('.overlay');
-console.log(overlay)
+const menuBtn = document.querySelector('.header .header-section .menu-button')
+const sidebar = document.querySelector('.sidebar')
+const overlay = document.querySelector('.overlay')
+const logoColor = document.querySelector('.logo-image img')
 
-menuBtn.addEventListener('click', function sidebarOpen() {
+function sidebarOpen() {
   menuBtn.classList.toggle('is-active')
   sidebar.classList.toggle('is-active')
   overlay.classList.toggle('is-active')
-})
+  logoColor.classList.toggle('logo-color')
+}
 
-overlay.addEventListener('click', function closeSidebar() {
+function closeSidebar() {
   menuBtn.classList.remove('is-active')
   sidebar.classList.remove('is-active')
   overlay.classList.remove('is-active')
-})
+  logoColor.classList.remove('logo-color')
+}
+
+menuBtn.addEventListener('click', sidebarOpen)
+
+overlay.addEventListener('click', closeSidebar)
